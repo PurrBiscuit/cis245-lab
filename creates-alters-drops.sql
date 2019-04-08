f95ada1d5f6e8ff0ff7da525c4b58e6ae060ec5d
Purrbiscuit <spurr@articulate.com>
2019-04-07 22:25:05 -0400

    add the last of the table creates

--- creates-alters-drops.sql
+++ creates-alters-drops.sql
@@ -98,3 +98,112 @@ CREATE TABLE EPISODE_WRITERS(
 	PersonID 	uniqueidentifier 	NOT NULL,
 	CONSTRAINT PK_EpisodeWriters PRIMARY KEY(EpisodeID,PersonID)
 );
+
+IF OBJECT_ID('dbo.AWARD_CATEGORY_RESULTS', 'U') IS NOT NULL
+DROP TABLE dbo.AWARD_CATEGORY_RESULTS;
+
+CREATE TABLE AWARD_CATEGORY_RESULTS(
+	AwardCategoryID 	uniqueidentifier 	NOT NULL,
+	ShowID 				uniqueidentifier 	NOT NULL,
+	PersonID 			uniqueidentifier	NULL,
+	Result 				bit 				NOT NULL,
+	ResultDate			date 				NOT NULL,
+	CONSTRAINT PK_AwardCategoryResults PRIMARY KEY(AwardCategoryID,ShowID)
+);
+
+IF OBJECT_ID('dbo.AWARD_CATEGORIES', 'U') IS NOT NULL
+DROP TABLE dbo.AWARD_CATEGORIES;
+
+CREATE TABLE AWARD_CATEGORIES(
+	AwardCategoryID 	uniqueidentifier 	NOT NULL,
+	AwardCategoryName 	varchar(200)		NOT NULL,
+	AwardID 			uniqueidentifier	NOT NULL,
+	CONSTRAINT PK_AwardCategoryID PRIMARY KEY(AwardCategoryID)
+);
+
+IF OBJECT_ID('dbo.SHOWS', 'U') IS NOT NULL
+DROP TABLE dbo.SHOWS;
+
+CREATE TABLE SHOWS(
+	ShowID 				uniqueidentifier 	NOT NULL,
+	ShowName 			varchar(150)	 	NOT NULL,
+	CountryOfOrigin 	varchar(2)			NOT NULL,
+	OriginalLanguage	varchar(3)			NOT NULL,
+	OriginalNetworkID 	uniqueidentifier	NOT NULL,
+	CONSTRAINT PK_ShowID PRIMARY KEY(ShowID)
+);
+
+IF OBJECT_ID('dbo.LOOKUP_LANGUAGES', 'U') IS NOT NULL
+DROP TABLE dbo.LOOKUP_LANGUAGES;
+
+CREATE TABLE LOOKUP_LANGUAGES(
+	LanguageISOCode 	varchar(3) 		NOT NULL,
+	LanguageName 		varchar(100) 	NOT NULL,
+	CONSTRAINT PK_LanguageISOCode PRIMARY KEY(LanguageISOCode)
+);
+
+IF OBJECT_ID('dbo.EPISODE_ROLES', 'U') IS NOT NULL
+DROP TABLE dbo.EPISODE_ROLES;
+
+CREATE TABLE EPISODE_ROLES(
+	EpisodeID 	uniqueidentifier 	NOT NULL,
+	RoleID 	 	uniqueidentifier 	NOT NULL,
+	CONSTRAINT PK_EpisodeRoles PRIMARY KEY(EpisodeID,RoleID)
+);
+
+IF OBJECT_ID('dbo.EPISODES', 'U') IS NOT NULL
+DROP TABLE dbo.EPISODES;
+
+CREATE TABLE EPISODES(
+	EpisodeID 				uniqueidentifier 	NOT NULL,
+	SeasonID				uniqueidentifier	NOT NULL,
+	EpisodeNumber			smallint			NOT NULL,
+	EpisodeTitle			varchar(150)		NOT NULL,
+	DirectedBy				uniqueidentifier	NOT NULL,
+	OriginalAirDate			date 				NOT NULL,
+	RunningTimeInMinutes	smallint			NOT NULL,
+	CONSTRAINT PK_EpisodeID PRIMARY KEY(EpisodeID)
+);
+
+IF OBJECT_ID('dbo.AWARDS', 'U') IS NOT NULL
+DROP TABLE dbo.AWARDS;
+
+CREATE TABLE AWARDS(
+	AwardID 			uniqueidentifier 	NOT NULL,
+	AwardName 			varchar(150)		NOT NULL,
+	AwardFounded		date 				NOT NULL,
+	AwardWebsite		varchar(250)		NULL,
+	CONSTRAINT PK_AwardID PRIMARY KEY(AwardID)
+);
+
+IF OBJECT_ID('dbo.SEASONS', 'U') IS NOT NULL
+DROP TABLE dbo.SEASONS;
+
+CREATE TABLE SEASONS(
+	SeasonID 			uniqueidentifier 	NOT NULL,
+	ShowID 				uniqueidentifier	NOT NULL,
+	SeasonNumber		tinyint 			NOT NULL,
+	FirstAiredDate		date 				NOT NULL,
+	LastAiredDate		date 				NULL,
+	NielsenRating 		smallint			NULL,
+	NielsenRatingRank	tinyint				NULL,
+	CONSTRAINT PK_SeasonID PRIMARY KEY(SeasonID)
+);
+
+IF OBJECT_ID('dbo.SHOW_GENRES', 'U') IS NOT NULL
+DROP TABLE dbo.SHOW_GENRES;
+
+CREATE TABLE SHOW_GENRES(
+	GenreID 	uniqueidentifier 	NOT NULL,
+	ShowID 		uniqueidentifier 	NOT NULL,
+	CONSTRAINT PK_ShowGenres PRIMARY KEY(GenreID,ShowID)
+);
+
+IF OBJECT_ID('dbo.LOOKUP_GENRES', 'U') IS NOT NULL
+DROP TABLE dbo.LOOKUP_GENRES;
+
+CREATE TABLE LOOKUP_GENRES(
+	GenreID 	uniqueidentifier 	NOT NULL,
+	Genre 		varchar(200) 		NOT NULL,
+	CONSTRAINT PK_GenreID PRIMARY KEY(GenreID)
+);
